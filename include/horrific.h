272b4007d1b347511214190934c8eb91a432208c
Alan Diniz <alanhdiniz@gmail.com>
2022-06-21 22:19:47 -0300

    Starting to implement the test app

---
+++ include/horrific.h
@@ -0,0 +1,15 @@
+#ifndef HORRIFIC_H_
+
+#define HORRIFIC_H_
+
+#include "core/game.h"
+#include "core/gameobj.h"
+#include "core/scene.h"
+#include "core/transform2D.h"
+#include "core/transform3D.h"
+
+#include "rendering/circlerenderer2D.h"
+#include "rendering/rectrenderer2D.h"
+#include "rendering/sprites.h"
+
+#endif
---
+++ test/include/test.h
@@ -0,0 +1,18 @@
+#ifndef TEST_H_
+
+#define TEST_H_
+
+#include "horrific.h"
+
+using namespace Horrific;
+
+namespace Test
+{
+    class TestGame : public Core::Game
+    {
+    public:
+    private:
+    };
+}
+
+#endif
